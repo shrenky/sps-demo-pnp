d5d8fc504cc1dd43eb349860ddc099b6f8b95e66
shrenky <shrenkyang@gmail.com>
2019-06-08 22:40:56 +0800

    Create PnP Demo project

---
+++ src/webparts/spspnpdemo/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpspnpdemoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SpspnpdemoWebPartStrings' {
+  const strings: ISpspnpdemoWebPartStrings;
+  export = strings;
+}
